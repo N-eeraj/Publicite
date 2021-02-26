f0ff693ad474ce90c71d2c7c28cbe8a0b28fcc47
N-eeraj <70063961+N-eeraj@users.noreply.github.com>
2021-02-26 14:17:48 +0530

    Added Home Section

--- script.js
+++ script.js
@@ -2,18 +2,26 @@ let menu=false;
 
 function fnShowMenu()
 {
+	console.log(menu)
 	if(menu==false)
 	{
 		document.getElementById('hamburger').setAttribute('src', 'images/close.png');
 		document.getElementById('menu_container').style.display = 'block';
-		menu=true
+		menu=true;
 	}
 	else
 	{
 		
 		document.getElementById('hamburger').setAttribute('src', 'images/hamburger.png');
 		document.getElementById('menu_container').style.display = 'none';
-		menu=false
+		menu=false;
 	}
 	
+}
+
+function fnInternalLink()
+{
+	menu=true;
+	console.log(menu)
+	fnShowMenu()
 }
