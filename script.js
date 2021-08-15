df7bc6db05edd486654833878dc5ae1cdeaa65f2
N-eeraj <neeraj7rajeena@gmail.com>
2021-08-15 17:39:52 +0530

    added tab with active

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
- 
+function switchTab(tab_name)
+{
+    document.getElementsByClassName("active-tab")[0].classList.remove("active-tab");
+    document.getElementById(tab_name).classList.add("active-tab");
+}
+
+switchTab("design_tab");
