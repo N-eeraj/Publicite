f3bef077f6d03b428a8841a92c1662d943b0ecd7
N-eeraj <neeraj7rajeena@gmail.com>
2021-10-28 22:58:16 +0530

    completed home section

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 $(document).ready(function () {
     
-    $('#menu').on('click', function () {
-       $('nav ul').toggleClass('open-menu');
-       $('#menu hr:first-child').toggleClass('first-hr');
-       $('#menu hr:nth-child(2)').toggleClass('second-hr');
-       $('#menu hr:last-child').toggleClass('last-hr');
-    });
+    window.onscroll = function() {
+        var nav = document.getElementById('nav');
+        if (window.pageYOffset > 60)
+            $('nav').addClass('nav-scroll');
+        else
+            $('nav').removeClass('nav-scroll');
+    }
+
+    $('#menu').on('click', menuToggle);
+
+    $('nav a').on('click', menuToggle);
 
 });
+
+const menuToggle = () => {
+    $('nav ul').toggleClass('open-menu');
+    $('#menu hr:first-child').toggleClass('first-hr');
+    $('#menu hr:nth-child(2)').toggleClass('second-hr');
+    $('#menu hr:last-child').toggleClass('last-hr');
+}
