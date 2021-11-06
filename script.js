9dd49d57b31cbe1f5a13dad0cdf23c648a065554
N-eeraj <neeraj7rajeena@gmail.com>
2021-11-06 12:52:22 +0530

    new: serives section & minor changes

--- script.js
+++ script.js
@@ -9,11 +9,22 @@ $(document).ready(function () {
     }
 
     $('#menu').on('click', menuToggle);
-    $('nav a').on('click', menuToggle);
+    $('nav a').on('click', function() {
+        $('.active-nav').removeClass('active-nav');
+        $(this).addClass('active-nav');
+        menuToggle();
+    });
+
+    $('#service button').on('click', function () {
+        $('.active-service').removeClass('active-service');
+        $('.carousel-active').removeClass('carousel-active');
+        $(this).addClass('active-service');
+        $(`div[data-carousel='${this.innerText}']`).addClass('carousel-active');
+    });
 
 });
 
-window.onload = () => $('#loader').css('transform', 'scaleY(0)');
+window.onload = () => $('#loader').css('transform', 'translateY(-100vh)');
 
 const menuToggle = () => {
     $('nav ul').toggleClass('open-menu');
